bf1f0036abf3174554c20acb073e5b1041be8b3d
luissd0ev <luissdev@outlook.com>
2024-07-10 12:21:14 -0600

    first commit

---
+++ ConsolaTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System; 
+class TestingExercise
+{
+    public static int suma(int x, int b)
+    {
+        return x + b;
+    }
+
+
+    public static int[] InvertValues(int[] input)
+    {
+        int[] test = new int[];
+       
+         input.Select((numero) => test.add( numero * (-1))); 
+        return test;
+    }
+    public static double NearestSq(int x)
+    {
+        double currentNumber = Math.Pow(Math.Round(Math.Sqrt(x)), 2);
+        Console.WriteLine("HELLO");
+        return currentNumber;
+    }
+
+    static void Main()
+    {
+        int[] numeros = new int[4] { 2, 3, -4, -7 };
+
+        int[] numberResults = InvertValues(numeros);
+        for(int i = 0; i < numeros.Length; i++)
+        {
+            Console.WriteLine(numeros[i]);
+        }
+
+        Console.WriteLine("Hello world");
+        Console.WriteLine(NearestSq(2)); 
+    }
+}
